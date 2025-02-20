d03a81aa71a5de077f3197e00b1865879d76abd9
Sumit Malviya <136370734+Mjlonir29@users.noreply.github.com>
2025-02-20 23:06:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function startGame() {
 
 function makeMove(cellIndex) {
   if (gameOver || gameBoard[cellIndex] !== '') return;
+  
 
   gameBoard[cellIndex] = currentPlayer;
   document.getElementById(`cell-${cellIndex}`).innerText = currentPlayer;
