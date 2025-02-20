45c61bd2430a1bbea9bec113164d9a678ff725e6
Sumit Malviya <136370734+Mjlonir29@users.noreply.github.com>
2025-02-20 23:06:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let gameOver = false;
 let scores = { X: 0, O: 0, Draw: 0 };
 
 
+
 function startGame() {
   document.getElementById('welcome-screen').classList.add('hidden');
   document.getElementById('game-board').classList.remove('hidden');
